79d900142d90f704798d0ac2a52b26accae85e71
alfonsotis <58032826+alfonsotis@users.noreply.github.com>
2023-07-31 12:54:08 +0200

    Managing Images in the Admin

--- store/admin.py
+++ store/admin.py
@@ -18,6 +18,15 @@ class InventoryFilter(admin.SimpleListFilter):
     def queryset(self, request, queryset: QuerySet):
         if self.value() == '<10':
             return queryset.filter(inventory__lt=10)
+        
+class ProductImageInline(admin.TabularInline):
+    model = models.ProductImage
+    readonly_fields = ['thumbnail']
+
+    def thumbnail(self, instance):
+        if instance.image.name != '':
+            return format_html(f'<img src="{instance.image.url}" class="thumbnail" />')
+        return 
 
 
 @admin.register(models.Product)
@@ -27,6 +36,7 @@ class ProductAdmin(admin.ModelAdmin):
         'slug': ['title']
     }
     actions = ['clear_inventory']
+    inlines = [ProductImageInline]
     list_display = ['title', 'unit_price',
                     'inventory_status', 'collection_title']
     list_editable = ['unit_price']
@@ -52,6 +62,12 @@ class ProductAdmin(admin.ModelAdmin):
             f'{updated_count} products were successfully updated.',
             messages.ERROR
         )
+
+    class Media:
+        css = {
+            'all': [ 'store/styles.css']
+        }
+
 
 
 @admin.register(models.Collection)
