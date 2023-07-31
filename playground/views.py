7b040e41296fc14d53e4311f926912d83c4637c9
alfonsotis <58032826+alfonsotis@users.noreply.github.com>
2023-07-31 15:05:55 +0200

    send_mail

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.core.mail import send_mail, mail_admins, BadHeaderError
 
 
 def say_hello(request):
-    return render(request, 'hello.html', {'name': 'Mosh'})
+
+    try:
+        send_mail('subject', 'message',
+                  'info@alfonso.com', ['bob@alfonso.com'])
+    except BadHeaderError:
+        pass
+
+    return render(request, 'hello.html', {'name': 'Super Alfonso'})
