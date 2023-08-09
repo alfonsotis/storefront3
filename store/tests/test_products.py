69d937061bb26273fa7f34b64373c8ac24fc1890
alfonsotis <58032826+alfonsotis@users.noreply.github.com>
2023-08-09 16:40:25 +0200

    test_if_data_is_invalid_returns_400

--- store/tests/test_products.py
+++ store/tests/test_products.py
@@ -25,6 +25,7 @@ class TestCreateProduct:
 
     def test_if_user_is_not_admin_returns_403(self, create_product, authenticate_user):
         authenticate_user(is_staff=False)
+
         response = create_product({
         "title": "a",
         "description": "b",
@@ -33,4 +34,19 @@ class TestCreateProduct:
         "unit_price": 1,
         "collection": 2
     })
+        
         assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_if_data_is_invalid_returns_400(self, create_product, authenticate_user):
+        authenticate_user(is_staff=True)
+
+        response = create_product({
+            "title": "a",
+            "description": "b",
+            "slug": "c",
+            "inventory": 2,
+            "unit_price": 1,
+            "collection": 2
+        })
+        
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
