2ddb260f677e313f22f7b07f3a4b64225173c373
alfonsotis <58032826+alfonsotis@users.noreply.github.com>
2023-08-09 19:27:48 +0200

    view products in Performance Test

--- locustfiles/browse_products.py
+++ locustfiles/browse_products.py
@@ -6,6 +6,7 @@ class Websiteuser(HttpUser):
 
     @task(2)
     def view_products(self):
+        print("View products")
         collection_id = randint(2,6)
         self.client.get(
             f'/store/products/?collection_id={collection_id}', 
@@ -13,6 +14,7 @@ class Websiteuser(HttpUser):
         
     @task(4)
     def view_product(self):
+        print('View product details')
         product_id = randint(1,1000)
         self.client.get(
             f'/store/products/?collection_id={product_id}',
@@ -20,6 +22,7 @@ class Websiteuser(HttpUser):
 
     @task(1)
     def add_to_cart(self):
+        print('add to cart')
         product_id=randint(1,10)
         self.client.post(
             f'/store/carts/{self.cart_id}/items/',
