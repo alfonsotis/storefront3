5910f338fc028c8ab8b112f9bdf7e666e9ece168
alfonsotis <58032826+alfonsotis@users.noreply.github.com>
2023-09-11 10:20:24 +0200

    add cache to say_hello view

--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
+from django.core.cache import cache
 from django.core.mail import send_mail, mail_admins, BadHeaderError, EmailMessage
 from templated_mail.mail import BaseEmailMessage
 import requests
 
 
 def say_hello(request):
-    requests.get('https://httpbin.org/delay/2')
+    key = 'httpbin_result'
+    if cache.get(key) is None:
+        response = requests.get('https://httpbin.org/delay/2')
+        data = response.json()
+        cache.set(key, data, 10*60)
     
-    return render(request, 'hello.html', {'name': 'Super Alfonso'})
+    return render(request, 'hello.html', {'name': cache.get(key)})
